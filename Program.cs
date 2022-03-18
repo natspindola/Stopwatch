96b7dbaf72ce3b8a136adbc75f046fbd9d8cb35c
natspindola <natspindola@hotmail.com>
2022-03-18 17:39:20 -0300

    substring

--- Program.cs
+++ Program.cs
@@ -13,10 +13,15 @@ namespace Stopwatch
         static void Menu()
         {
             Console.Clear();
-            Console.WriteLine("S = Segundo");
-            Console.WriteLine("M = Minuto");
+            Console.WriteLine("S = Segundo => 10s = 10 segundos");
+            Console.WriteLine("M = Minuto => 1m = 1 minuto");
             Console.WriteLine("0 = Sair");
             Console.WriteLine("Quanto tempo deseja contar?");
+
+            string data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+            Console.WriteLine(data);
+            Console.WriteLine(type);
         }
 
         static void Start(int time)
